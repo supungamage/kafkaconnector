009afb430791887e9e8e18fd334ab2ed4f693d9b
supun_g <supun.g@synergenhealth.com>
2022-11-01 08:15:38 +0530

    feature2 changed message 1

--- src/main/java/com/supung/kafkaconnector/controller/MessageController.java
+++ src/main/java/com/supung/kafkaconnector/controller/MessageController.java
@@ -17,6 +17,6 @@ public class MessageController {
     @PostMapping
     public String postMessage(BdeInput message) {
         messageService.postMessage(message);
-        return "message pushed to topic";
+        return "message pushed to topic new";
     }
 }
